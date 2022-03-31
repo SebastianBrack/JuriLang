43994ed8bc8783ace00141fb7dae3baa2320b2b7
Sebastian Brack <sbrack3@gmail.com>
2022-03-31 17:26:08 +0200

    fixed repl throwing away parser context

--- Juri/Repl.fs
+++ Juri/Repl.fs
@@ -2,37 +2,38 @@ module Juri.Internal.Repl
 
 open Interpreter
 open CoreLib
-open Juri.Internal.OutputWriter
-open Juri.Internal.Parser
-open Juri.Internal.Runtime
+open OutputWriter
 open Runtime
 open Parser
 open ParserCombinators
 
 
 
-let rec private repl (outputWriter: IOutputWriter) (state: ComputationState) =
+let rec private repl (outputWriter: IOutputWriter) (parserContext: JuriContext) (state: ComputationState) =
     let mutable userInput = ""
     let mutable line = stdin.ReadLine()
     while not (line.EndsWith(";")) do
         userInput <- userInput + line + "\n"
         line <- stdin.ReadLine()
     userInput <- userInput + line.TrimEnd(';') + "\n"
-    let stream = CharStream(userInput, JuriContext.Default)
+    let stream = CharStream(userInput, parserContext)
     match stream.RunParser(juriProgram) with
-    | Success (r,_,_) ->
+    | Success (r,c,_) ->
         compute r outputWriter state
-        >>= repl outputWriter
+        >>= repl outputWriter c
     | Failure (msg, _) ->
+        let oldContext = stream.GetContext()
         outputWriter.WriteERR(msg, stream.GetContext().Line)
-        repl outputWriter state
+        repl outputWriter oldContext state 
     | Fatal (msg, _) ->
+        let oldContext = stream.GetContext()
         outputWriter.WriteERR(msg, stream.GetContext().Line)
-        repl outputWriter state
+        repl outputWriter oldContext state
 
 
 
 let startRepl () =
     let outputWriter = ConsoleWriter()
     let initialState = { ComputationState.Default with Environment = createEnvWithCoreLibFunctions () }
-    repl outputWriter initialState
+    let initialParserContext = JuriContext.Default
+    repl outputWriter initialParserContext initialState 
